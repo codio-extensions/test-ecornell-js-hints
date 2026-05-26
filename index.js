e3e68a8643bf0863df4cb94ff98c17cd87e351ce
mohitchandarana <44833060+mohitchandarana@users.noreply.github.com>
2026-05-26 12:50:46 -0400

    JS hints extension test code

---
+++ index.js
@@ -0,0 +1,27 @@
+// JS Test
+(async function (codioIDE, window) {
+
+  codioIDE.coachBot.register(
+    "customHintsJSv2",
+    "JS hint button",
+    onButtonPress
+  );
+
+  async function onButtonPress() {
+    codioIDE.coachBot.showThinkingAnimation();
+    console.info("[JS Hint v2] getContext:start");
+    const context = await codioIDE.coachBot.getContext();
+    console.info("[Jupyter Hint v2] getContext:success", summarizeContext(context));
+
+    const studentFile = context.files[0].content
+    const guidePage = content.guidesPage.content
+    
+    const locatorResult = await codioIDE.coachBot.ask({
+      systemPrompt: "You're a helpful assistant",
+      userPrompt: "{% prompt 'AGENT_LOCATOR_COACH_JS' %}",
+      vars: {
+        STUDENT_FILE: studentFile
+      }
+    }
+  }
+}
